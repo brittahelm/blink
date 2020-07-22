647b3bfc1d22effc71f2b9b76bee585e606f5b59
Britta Helm <brittahelm@gmx.net>
2020-07-22 11:19:48 +0200

    adjusted score for responsiveness

--- script.js
+++ script.js
@@ -66,6 +66,9 @@ function displayMoves() {
     ctx.clearRect(50, 525, 150, 100);
     ctx.fillStyle = 'white';
     ctx.font = "20px Montserrat";
+    if (mql.matches) {
+        ctx.font = "25px Montserrat";
+    }
     ctx.beginPath();
     ctx.fillText(`moves: ${moves}`, 50, 550);
 }
@@ -73,11 +76,20 @@ function displayMoves() {
 // show sound switch on screen
 function displaySoundSwitch(state) {
     ctx.beginPath();
-    ctx.clearRect(430, 525, 150, 100);
-    ctx.fillStyle = 'white';
-    ctx.font = "12px Montserrat";
-    ctx.beginPath();
-    ctx.fillText(`turn sound ${state}`, 430, 550);
+    if (mql.matches) {
+        ctx.clearRect(400, 525, 150, 100);
+        ctx.fillStyle = 'white';
+        ctx.font = "25px Montserrat";
+        ctx.beginPath();
+        ctx.fillText(`sound ${state}`, 400, 550);
+    }
+    else {
+        ctx.clearRect(430, 525, 150, 100);
+        ctx.fillStyle = 'white';
+        ctx.font = "12px Montserrat";
+        ctx.beginPath();
+        ctx.fillText(`turn sound ${state}`, 430, 550);
+    }
 }
 
 // create sound off/on switch
